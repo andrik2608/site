b7fec8c04badfe22ebdc13e001fa14855dafceef
andrik2608 <69986258+andrik2608@users.noreply.github.com>
2020-08-30 22:23:33 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.slider').slick({
+  arrows:true,
+  dots:false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+});
+$('.slider-lic-1').slick({
+  arrows:true,
+  dots:false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
+$('.slider-lic-2').slick({
+  arrows:true,
+  dots:false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
